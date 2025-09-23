abb0b3806f164c3dbb8347090ed54b6f66c7a75a
BrunoKrause <brunonoi05@gmail.com>
2025-09-22 22:00:25 -0300

    Adicionados arquivos referentes a aula 4

---
+++ Aulas/Aula 4/Exemplo 1/script.js
@@ -0,0 +1,14 @@
+/* Exemplo 1
+
+Bruno Krause*/
+// Comentário de uma linha
+/*
+console.log("Hello World Class 25-2N")
+console.log("Hello Pleople")
+
+prompt("Qual é o seu nome?")*/
+
+const nome = "Bruno"
+const idade = 20
+
+console.log("O nome é: ",nome, "e sua idade é: ",idade)
---
+++ Aulas/Aula 4/Exercícios/script.js
@@ -0,0 +1,6 @@
+const nome = "Bruno"
+const sobreNome = "Krause"
+const idade = 20
+const souUmEstudante = true
+
+console.log("Meu nome é:",nome, "Meu sobrenome é:", sobreNome, "Minha idade é:", idade, "Sou estudante?", souUmEstudante)
