16c8a88dee067c8a7ce469de4b34f6d1b267277d
BrunoKrause <brunonoi05@gmail.com>
2025-10-08 20:24:14 -0300

    Exercícios de fixação e desafio aula 9

---
+++ Aulas/Aula8/Exercícios Físicos/Desafio/script.js
@@ -0,0 +1,8 @@
+//1
+const frase = prompt("Escreva uma frase")
+let array = frase.split(" ")
+console.log(array)
+
+//2
+let array2 = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+console.log("O índice da palavra 'Abacaxi' é:", array2.indexOf("Abacaxi"),"E tem:", (array2.length), "itens.")
--- Aulas/Aula8/Exercícios Físicos/script.js
+++ Aulas/Aula8/Exercícios Físicos/script.js
@@ -19,7 +19,39 @@ const valor = array[i+6] //Cria a variável "valor" com o valor "9"
 console.log('f. ', valor) //Imprime f. 9
 
 //2
-const frase = prompt("Digite uma frase")
+const frase = prompt("Digite uma frase") //Abre uma caixa para o Usuário preencher
 
-console.log(frase.toUpperCase().replaceAll("A","I"), frase,length)
-//Qual será o
+console.log(frase.toUpperCase().replaceAll("A","I"), (frase.length)) //Imprime a frase do usuário com todas as letras Maiúsculas e trocando o "A" pelo "I" além de imprimir o tamanho(Quantidade de letras, contando espaços)
+//Qual será o valor impresso no console se a entrada do usuário for: "Subi num ônibus em Marrocos"
+//"SUBI NUM ÔNIBUS EM MIRROCOS" "27"
+
+//3
+const nome = prompt("Digite seu nome")
+const email = prompt("Digite seu email")
+const frase1 = `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`
+console.log(frase1)
+
+//4
+let comidasFavoritas = ["Lasanha", "Pizza", "Hambúrguer", "Churrasco", "Batata Frita"]
+console.log(comidasFavoritas)
+console.log("Essas são as minhas comidas preferidas:")
+console.log(comidasFavoritas[0])
+console.log(comidasFavoritas[1])
+console.log(comidasFavoritas[2])
+console.log(comidasFavoritas[3])
+console.log(comidasFavoritas[4])
+
+const comidaUsuario = prompt("Qual sua comida favorita?")
+comidasFavoritas[1] = comidaUsuario
+console.log(comidasFavoritas)
+
+//5
+let listaDeTarefas = []
+const tarefa1 = prompt("Diga uma tarefa que precise fazer:")
+const tarefa2 = prompt("Diga outra tarefa:")
+const tarefa3 = prompt("Agora a última:")
+listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+console.log(listaDeTarefas)
+const tarefaFeita = Number(prompt("Qual das três tarefas já finalizou? Digite o índice de 0, 1 ou 2."))
+listaDeTarefas.splice(tarefaFeita, 1)
+console.log(listaDeTarefas)
